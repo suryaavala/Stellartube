f397f17eb5dfa43d3b55a23891f4d3291a71a17f
Egene <geney.1997@gmail.com>
2018-04-09 20:07:06 +1000

    Added login manager

--- app/routes.py
+++ app/routes.py
@@ -1,17 +1,30 @@
-from flask import Flask, render_template, redirect, request, url_for
+from flask import Flask, render_template, redirect, request, url_for, abort
 from app import app
+from .forms import SignInForm
+from .login_manager import User
+
 
 @app.route('/', methods=['get', 'post'])
 def index():
-    return '''
-<html>
-  <head>
-    <title>Sign In</title>
-  </head>
-  <body>
-    <h1>Sign In</h1>
-    <p>username:</p>
-    <p>password:</p>
-  </body>
-</html>
-'''
+    return render_template('index.html')
+
+@app.route('/signin', methods=['get', 'post'])
+def signin():
+    signinform = SignInForm()
+
+    # Check sign in attempt
+    if signinform.validate_on_submit():
+        username = signinform.username.data
+        password = signinform.password.data
+
+        if username == password:
+            user = User(username)
+            login_user(user)
+
+            next = request.args.get('next')
+            #if not is_safe_url(next):
+            #    return abort(400)
+
+            return redirect(next or url_for('index'))
+
+    return render_template('sign_in.html', form=signinform)
