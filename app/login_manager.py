2b3561cead585e01404e9e76870bad5afab547a2
Egene <geney.1997@gmail.com>
2018-04-10 17:10:11 +1000

    Integrated with database

--- app/login_manager.py
+++ app/login_manager.py
@@ -1,10 +1,6 @@
-from app import app
-import flask_login
+from flask_login import UserMixin
 
-login_manager = flask_login.LoginManager()
-login_manager.init_app(app)
-
-class User(flask_login.UserMixin):
+class User(UserMixin):
     
     def __init__(self, id):
         self.id = id
