2748444bfc619887a56410b824e9bd54ace5defa
Egene <geney.1997@gmail.com>
2018-05-08 18:29:30 +1000

    Added upload and search forms

--- app/forms.py
+++ app/forms.py
@@ -1,9 +1,10 @@
 from flask_wtf import FlaskForm
 import wtforms as wtf
+import flask_wtf.file as fwtff
 
 class SignInForm(FlaskForm):
-    username = wtf.StringField(
-        'username',
+    email = wtf.StringField(
+        'email',
         validators=[wtf.validators.DataRequired()]
     )
     password = wtf.PasswordField(
@@ -14,8 +15,16 @@ class SignInForm(FlaskForm):
     submit = wtf.SubmitField('submit')
 
 class SignUpForm(FlaskForm):
-    username = wtf.StringField(
-        'username',
+    first_name = wtf.StringField(
+        'fname',
+        validators=[wtf.validators.DataRequired()]
+    )
+    last_name = wtf.StringField(
+        'lname',
+        validators=[wtf.validators.DataRequired()]
+    )
+    email = wtf.StringField(
+        'email',
         validators=[wtf.validators.DataRequired()]
     )
     password = wtf.PasswordField(
@@ -23,8 +32,36 @@ class SignUpForm(FlaskForm):
         validators=[wtf.validators.DataRequired()]
     )
     confirm_password = wtf.PasswordField('confirm password')
-    address = wtf.StringField(
-        'address',
+    submit = wtf.SubmitField('submit')
+
+class UploadVideoForm(FlaskForm):
+    title = wtf.StringField(
+        'title',
+        validators=[wtf.validators.DataRequired()]
+    )
+    description = wtf.TextAreaField(
+        'description',
+        validators=[wtf.validators.DataRequired()]
+    )
+    price = wtf.DecimalField(
+        'price',
+        validators=[wtf.validators.DataRequired()]
+    )
+    labels = wtf.StringField(
+        'labels',
+        validators=[wtf.validators.DataRequired()]
+    )
+    video_file = fwtff.FileField(
+        'vfile',
+        validators=[
+            fwtff.FileRequired(),
+            fwtff.FileAllowed(['mp4'])
+        ]
+    )
+    submit = wtf.SubmitField('submit')
+
+class SearchForm(FlaskForm):
+    search_string = wtf.StringField('search_string',
         validators=[wtf.validators.DataRequired()]
     )
     submit = wtf.SubmitField('submit')
