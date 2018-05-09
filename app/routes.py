076dc703c3aa3a0b5341dd7ed63b257deb4d72b0
Egene <geney.1997@gmail.com>
2018-05-10 04:17:36 +1000

    added user library route. changed search route to use queries from get request instead of post requests. cleaned up code.

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, redirect, request, url_for, abort, flash, send_from_directory
+from flask import Markup as fm
 from app import app
 from app import models as mdl
 from app import forms as frm
@@ -9,7 +10,7 @@ import flask_login as fl
 import os
 
 # Specify login view for login manager
-lm.lm.login_view = 'login'
+lm.lm.login_view = 'signin'
 
 
 # Homepage
@@ -18,21 +19,10 @@ lm.lm.login_view = 'login'
 # to their account details, link to the upload page, and access the help page
 @app.route('/', methods=['get', 'post'])
 def index():
-    videos = []
-    vsearch = list(map(int, db.sql_searchVideos('', 100)))
-    for v in vsearch:
-        vinfo = db.sql_getVideo(v)
-        videos.append(mdl.videoInfo(v, int(vinfo[0]), vinfo[1], vinfo[2], 
-            vinfo[3], vinfo[4], 'uploads/'+vinfo[5],
-            url_for('static',filename='images/blockmart_logo.svg'))
-        )
-    
-    searchform = frm.SearchForm()
-    if searchform.validate_on_submit():
-        print(searchform.search_string.data)
-        return redirect(url_for('search', search_string=searchform.search_string.data))
+    v_ids = list(map(int, db.sql_searchVideos('', 100)))
+    videos = getVideosFromList(v_ids)
 
-    return render_template('index.html', videos=videos, search_form=searchform)
+    return render_template('index.html', videos=videos)
 
 # Sign In Page
 # Page allows users to sign into their account if they have one, or go to the
@@ -89,10 +79,6 @@ def signup():
 def watch_video(video_id):
     video = db.sql_getVideo(video_id)
     
-    searchform = frm.SearchForm()
-    if searchform.validate_on_submit():
-        return redirect(url_for('search', search_string=searchform.search_string.data))
-
     return render_template(
         'view_video.html',
         video_id=video_id,
@@ -101,9 +87,9 @@ def watch_video(video_id):
         video_path=url_for('static', filename='videos/%s'%video[5]),
         videoPrice = video[4],
         videoIsPublic = True if float(video[4]) == 0 else False,
-        videoIsOwned = True if str(fl.current_user.id) == str(video[0]) else False,
-        videoIsPurchased = False,
-        search_form=searchform)
+        videoIsOwned = True if fl.current_user.is_authenticated and 
+            str(fl.current_user.id) == str(video[0]) else False,
+        videoIsPurchased = False)
 
 @app.route('/logout', methods=['get', 'post'])
 @fl.login_required
@@ -115,7 +101,6 @@ def logout():
 @fl.login_required
 def upload():
     uploadform = frm.UploadVideoForm()
-    searchform = frm.SearchForm()
 
     if uploadform.validate_on_submit():
         vtitle = uploadform.title.data
@@ -132,12 +117,8 @@ def upload():
         
         if not hasUploaded:
             return redirect(url_for('index'))
- 
-    searchform = frm.SearchForm()
-    if searchform.validate_on_submit():
-        return redirect(url_for('search', search_string=searchform.search_string.data))
 
-    return render_template('upload_video.html', form=uploadform, search_form=searchform)
+    return render_template('upload_video.html', form=uploadform)
 
 @app.route('/download/<int:video_id>', methods=['get', 'post'])
 def download(video_id):
@@ -150,19 +131,37 @@ def download(video_id):
     else:
         return redirect(url_for('index'))
 
-@app.route('/search/<string:search_string>', methods=['get', 'post'])
-def search(search_string):
-    videos = []
-    vsearch = list(map(int, db.sql_searchVideos(search_string, 100)))
-    for v in vsearch:
-        vinfo = db.sql_getVideo(v)
-        videos.append(mdl.videoInfo(v, int(vinfo[0]), vinfo[1], vinfo[2], 
-            vinfo[3], vinfo[4], 'uploads/'+vinfo[5],
-            url_for('static',filename='images/blockmart_logo.svg'))
-        )
-    
-    searchform = frm.SearchForm()
-    if searchform.validate_on_submit():
-        return redirect(url_for('search', search_string=searchform.search_string.data))
+@app.route('/search', methods=['get'])
+def search():
+    if request.method == 'GET':
+        search_query = request.args.get('search_query', None)
+        if search_query:
+            v_ids = list(map(int, db.sql_searchVideos(search_query, 1000)))
+            videos = getVideosFromList(v_ids)
 
-    return render_template('search_results.html', videos=videos, search_form=searchform)
+            return render_template('search_results.html', videos=videos,
+                search_query=search_query)
+
+    return redirect(url_for('index'))
+
+@app.route('/mylibrary', methods=['get', 'post'])
+@fl.login_required
+def user_library():
+    v_ids = db.sql_getUserVideos(fl.current_user.id)
+    videos = getVideosFromList(v_ids)
+    return render_template('user_library.html', videos=videos)
+
+# Helper Functions
+
+# Function: getVideosFromList(v_ids)
+# Takes an array of video IDs and returns a list of videoInfo objects
+def getVideosFromList(v_ids):
+    videos = []
+    for v in v_ids:
+        vi = db.sql_getVideo(v)
+        videos.append(mdl.videoInfo(v, int(vi[0]),
+            vi[1], vi[2], vi[3], vi[4], 'uploads/'+vi[5], 
+            url_for('static', filename='images/blockmart_logo.svg'))
+        )
+
+    return videos
